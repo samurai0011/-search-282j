753db9b0beaf915722e55e1a13945436971455f6
samurai0011 <hsen935173@gmail.com>
2025-05-22 14:12:47 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,10 @@
+#𝙎𝘼𝙈𝙐𝙍𝘼𝙄 𝘽𝙊𝙏𝙎
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("22750400", "")
+API_HASH  = os.environ.get("a0213f361ff290f021156c8dad30195b", "")
+BOT_TOKEN = os.environ.get("7713969480:AAFUorAezRrAm-3gTxwnzntwd1ZZbAJY_24", "") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
